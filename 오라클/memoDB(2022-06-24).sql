cce27f55286f2c97afb308fbf3a76194e6a647fe
lee9805 <dlthwjd1161@naver.com>
2022-06-24 17:44:20 +0900

    과제 one day project

---
+++ 오라클/memoDB(2022-06-24).sql
@@ -0,0 +1,14 @@
+CREATE TABLE tbl_memo (
+		m_seq NUMBER PRIMARY KEY,
+		m_author VARCHAR2(25) NOT NULL,
+		m_date VARCHAR2(10) NOT NULL,
+		m_time VARCHAR2(10) NOT NULL,
+		m_memo nVARCHAR2(400) NOT NULL,
+		m_image nVARCHAR2(125)
+);
+DROP TABLE tbl_memo;
+
+CREATE SEQUENCE seq_memo
+INCREMENT BY 1 START WITH 1;
+
+DROP SEQUENCE seq_bbs;
---
+++ 오라클/관리자(2022-06-24).sql
@@ -0,0 +1,8 @@
+CREATE TABLESPACE memoDB
+datafile 'C:/oraclexe/data/memo.dbf'
+size 1m AUTOEXTEND on next 1k;
+
+create user memoDB IDENTIFIED BY 12341234
+default tablespace memoDB;
+
+GRANT DBA to memoDB;
